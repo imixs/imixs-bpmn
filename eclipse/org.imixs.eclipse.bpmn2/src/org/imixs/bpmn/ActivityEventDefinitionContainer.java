e418e1f88f431bf2a90749bf174a2f304a6c016e
Ralph Soika <ralph.soika@imixs.com>
2015-03-30 23:09:18 +0200

    issue #8

--- eclipse/org.imixs.eclipse.bpmn2/src/org/imixs/bpmn/ActivityEventDefinitionContainer.java
+++ eclipse/org.imixs.eclipse.bpmn2/src/org/imixs/bpmn/ActivityEventDefinitionContainer.java
@@ -4,15 +4,33 @@ import org.eclipse.bpmn2.BaseElement;
 import org.eclipse.bpmn2.Bpmn2Factory;
 import org.eclipse.bpmn2.ExtensionAttributeValue;
 import org.eclipse.bpmn2.IntermediateCatchEvent;
+import org.eclipse.bpmn2.Task;
 import org.eclipse.bpmn2.modeler.core.features.CustomShapeFeatureContainer;
 import org.eclipse.bpmn2.modeler.core.features.ShowPropertiesFeature;
+import org.eclipse.bpmn2.modeler.core.features.activity.task.AddTaskFeature;
 import org.eclipse.bpmn2.modeler.core.model.ModelDecorator;
+import org.eclipse.bpmn2.modeler.core.preferences.ShapeStyle;
+import org.eclipse.bpmn2.modeler.core.utils.BusinessObjectUtil;
+import org.eclipse.bpmn2.modeler.core.utils.StyleUtil;
+import org.eclipse.bpmn2.modeler.ui.features.activity.task.TaskFeatureContainer;
+import org.eclipse.bpmn2.modeler.ui.features.activity.task.TaskFeatureContainer.CreateTaskFeature;
+import org.eclipse.bpmn2.modeler.ui.features.event.IntermediateCatchEventFeatureContainer;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.impl.EAttributeImpl;
+import org.eclipse.graphiti.features.IAddFeature;
+import org.eclipse.graphiti.features.ICreateFeature;
 import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.IAddContext;
 import org.eclipse.graphiti.features.custom.ICustomFeature;
+import org.eclipse.graphiti.mm.pictograms.ContainerShape;
+import org.eclipse.graphiti.mm.pictograms.Shape;
+import org.eclipse.graphiti.services.Graphiti;
+import org.eclipse.graphiti.services.IGaService;
+import org.eclipse.graphiti.services.IPeService;
+import org.eclipse.graphiti.util.ColorConstant;
+import org.eclipse.graphiti.util.IColorConstant;
 import org.imixs.bpmn.model.ModelPackage;
 
 public class ActivityEventDefinitionContainer extends
@@ -23,6 +41,9 @@ public class ActivityEventDefinitionContainer extends
 	public final static String ACTIVITYNTITY_TASK_ID = "org.imixs.workflow.bpmn.ActivityEntityEvent";
 	final static EStructuralFeature METADATA_FEATURE = ModelPackage.eINSTANCE.getDocumentRoot_Property();
 
+	private static final IColorConstant ACTIVITYENTITY_BACKGROUND = new ColorConstant(255, 217, 64);
+
+	
 	/**
 	 * This method inspects the object to determine what its custom task ID
 	 * should be. In this case, we check the namespace of the "type" attribute.
@@ -54,11 +75,75 @@ public class ActivityEventDefinitionContainer extends
 
 	@Override
 	public ICustomFeature[] getCustomFeatures(IFeatureProvider fp) {
-		return new ICustomFeature[] { new ShowPropertiesFeature(fp) };
+		//return new ICustomFeature[] { new ShowPropertiesFeature(fp) };
+		return super.getCustomFeatures(fp);
 	}
 
 	
 	
+	
+	@Override
+	protected IntermediateCatchEventFeatureContainer createFeatureContainer(IFeatureProvider fp) {
+		return new IntermediateCatchEventFeatureContainer() {
+
+			
+			/**
+			 *  override the Add Feature from the chosen Feature Container
+			 *  base class . Typically you will want to override the decorateShape() method
+			 *   which allows you to customize the graphical representation of this Custom Task figure.
+			 */
+			@Override
+			public IAddFeature getAddFeature(IFeatureProvider fp) {
+				return new AddIntermediateCatchEventFeature(fp) {
+
+					
+					@Override
+					protected void decorateShape(IAddContext context, ContainerShape containerShape, IntermediateCatchEvent businessObject) {
+						super.decorateShape(context, containerShape, businessObject);
+						
+						
+						setFillColor(containerShape);
+						
+
+					}
+				};
+			}
+
+			
+			/**
+			 *  this MUST be overridden if you intend to add extension attributes to
+			 *   your business object (bpmn2 element) - see the code example below. You will also want to
+			 *   provide your own images for the tool palette by overriding getCreateImageId() and
+			 *   getCreateLargeImageId() in your Create Feature.
+			 */
+			@Override
+			public ICreateFeature getCreateFeature(IFeatureProvider fp) {
+				return new CreateIntermediateCatchEventFeature(fp) {
+				};
+			}
+
+
+			/**
+			 * Common method used to set the fill color for Imixs CustomTask figure.
+			 * This method is called by both the CreateFeature and the UpdateFeature.
+			
+			 * @param containerShape - the ContainerShape that corresponds to the Task.
+			 */
+			private void setFillColor(ContainerShape containerShape) {
+				IntermediateCatchEvent ta = BusinessObjectUtil.getFirstElementOfType(containerShape, IntermediateCatchEvent.class);
+				if (ta!=null) {
+					Shape shape = containerShape.getChildren().get(0);
+					ShapeStyle ss = new ShapeStyle();
+					
+					
+					
+					ss.setDefaultColors(ACTIVITYENTITY_BACKGROUND);
+					StyleUtil.applyStyle(shape.getGraphicsAlgorithm(), ta, ss);
+				}
+			}
+			
+		};
+	}
 	
 	
 }
--- eclipse/org.imixs.eclipse.bpmn2/src/org/imixs/bpmn/ImixsTaskFeatureContainer.java
+++ eclipse/org.imixs.eclipse.bpmn2/src/org/imixs/bpmn/ImixsTaskFeatureContainer.java
@@ -1,15 +1,46 @@
 package org.imixs.bpmn;
 
+import org.eclipse.bpmn2.Task;
+import org.eclipse.bpmn2.TextAnnotation;
+import org.eclipse.bpmn2.modeler.core.adapters.ExtendedPropertiesAdapter;
 import org.eclipse.bpmn2.modeler.core.features.CustomShapeFeatureContainer;
-import org.eclipse.bpmn2.modeler.core.features.ShowPropertiesFeature;
+import org.eclipse.bpmn2.modeler.core.features.MultiUpdateFeature;
+import org.eclipse.bpmn2.modeler.core.features.activity.task.AddTaskFeature;
+import org.eclipse.bpmn2.modeler.core.features.artifact.UpdateTextAnnotationFeature;
+import org.eclipse.bpmn2.modeler.core.features.label.UpdateLabelFeature;
 import org.eclipse.bpmn2.modeler.core.model.ModelDecorator;
+import org.eclipse.bpmn2.modeler.core.preferences.ShapeStyle;
+import org.eclipse.bpmn2.modeler.core.utils.BusinessObjectUtil;
+import org.eclipse.bpmn2.modeler.core.utils.StyleUtil;
+import org.eclipse.bpmn2.modeler.ui.features.activity.task.TaskFeatureContainer;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.impl.EAttributeImpl;
+import org.eclipse.graphiti.features.IAddFeature;
+import org.eclipse.graphiti.features.ICreateFeature;
+import org.eclipse.graphiti.features.IDirectEditingFeature;
 import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.IReason;
+import org.eclipse.graphiti.features.IResizeShapeFeature;
+import org.eclipse.graphiti.features.IUpdateFeature;
+import org.eclipse.graphiti.features.context.IAddContext;
+import org.eclipse.graphiti.features.context.ICreateContext;
+import org.eclipse.graphiti.features.context.IUpdateContext;
 import org.eclipse.graphiti.features.custom.ICustomFeature;
-
+import org.eclipse.graphiti.features.impl.Reason;
+import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
+import org.eclipse.graphiti.mm.algorithms.Image;
+import org.eclipse.graphiti.mm.algorithms.Rectangle;
+import org.eclipse.graphiti.mm.algorithms.RoundedRectangle;
+import org.eclipse.graphiti.mm.pictograms.ContainerShape;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.eclipse.graphiti.mm.pictograms.Shape;
+import org.eclipse.graphiti.services.Graphiti;
+import org.eclipse.graphiti.services.IGaService;
+import org.eclipse.graphiti.services.IPeService;
+import org.eclipse.graphiti.util.ColorConstant;
+import org.eclipse.graphiti.util.IColorConstant;
 /**
  * Imixs ProcessEntity task container
  * 
@@ -20,7 +51,16 @@ public class ImixsTaskFeatureContainer extends CustomShapeFeatureContainer {
 
 	// these values must match what's in the plugin.xml
 	public final static String PROCESSENTITY_TASK_ID = "org.imixs.workflow.bpmn.ProcessEntityTask";
+//	private static final IColorConstant PROCESSENTITY_BACKGROUND = new ColorConstant(255, 204, 0);
 
+	// ok
+//	private static final IColorConstant PROCESSENTITY_BACKGROUND = new ColorConstant(255, 217, 64);
+
+
+	private static final IColorConstant PROCESSENTITY_BACKGROUND = new ColorConstant(144, 176, 224);
+//	private static final IColorConstant PROCESSENTITY_BACKGROUND = new ColorConstant(166, 189, 224);
+
+	
 	/**
 	 * This method inspects the object to determine what its custom task ID
 	 * should be. In this case, we check the namespace of the "type" attribute.
@@ -53,6 +93,84 @@ public class ImixsTaskFeatureContainer extends CustomShapeFeatureContainer {
 	}
 
 
+	
+	
+
+	@Override
+	protected TaskFeatureContainer createFeatureContainer(IFeatureProvider fp) {
+		return new TaskFeatureContainer() {
+
+			
+			/**
+			 *  override the Add Feature from the chosen Feature Container
+			 *  base class . Typically you will want to override the decorateShape() method
+			 *   which allows you to customize the graphical representation of this Custom Task figure.
+			 */
+			@Override
+			public IAddFeature getAddFeature(IFeatureProvider fp) {
+				return new AddTaskFeature(fp) {
+
+					
+					@Override
+					protected void decorateShape(IAddContext context, ContainerShape containerShape, Task businessObject) {
+						super.decorateShape(context, containerShape, businessObject);
+						
+						IGaService gaService = Graphiti.getGaService();
+						IPeService peService = Graphiti.getPeService();
+						// Change the size of the default TextAnnotation selection rectangle
+						// Rectangle selectionRect =
+						// (Rectangle)containerShape.getGraphicsAlgorithm();
+						// int width = 140;
+						// int height = 60;
+						// selectionRect.setWidth(width);
+						// selectionRect.setHeight(height);
+				
+						
+						setFillColor(containerShape);
+						
+
+					}
+				};
+			}
+
+			
+			/**
+			 *  this MUST be overridden if you intend to add extension attributes to
+			 *   your business object (bpmn2 element) - see the code example below. You will also want to
+			 *   provide your own images for the tool palette by overriding getCreateImageId() and
+			 *   getCreateLargeImageId() in your Create Feature.
+			 */
+			@Override
+			public ICreateFeature getCreateFeature(IFeatureProvider fp) {
+				return new CreateTaskFeature(fp) {
+				};
+			}
+
+
+			/**
+			 * Common method used to set the fill color for Imixs CustomTask figure.
+			 * This method is called by both the CreateFeature and the UpdateFeature.
+			
+			 * @param containerShape - the ContainerShape that corresponds to the Task.
+			 */
+			private void setFillColor(ContainerShape containerShape) {
+				Task ta = BusinessObjectUtil.getFirstElementOfType(containerShape, Task.class);
+				if (ta!=null) {
+					//ExtendedPropertiesAdapter adapter = ExtendedPropertiesAdapter.adapt(ta);
+					//Boolean attributeValue = (Boolean)adapter.getFeatureDescriptor("evaluate").getValue();
+					Shape shape = containerShape.getChildren().get(0);
+					ShapeStyle ss = new ShapeStyle();
+					
+					
+					
+					ss.setDefaultColors(PROCESSENTITY_BACKGROUND);
+					StyleUtil.applyStyle(shape.getGraphicsAlgorithm(), ta, ss);
+					//Graphiti.getPeService().setPropertyValue(containerShape, "evaluate.property", propertyValue);
+				}
+			}
+			
+		};
+	}
 	
 	
 }
