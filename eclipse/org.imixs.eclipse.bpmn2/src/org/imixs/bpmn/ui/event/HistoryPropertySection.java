67410f6c788301f4de570e6c7ad5369cef73fe64
Ralph Soika <ralph.soika@imixs.com>
2015-04-03 17:39:54 +0200

    issue #15

---
+++ eclipse/org.imixs.eclipse.bpmn2/src/org/imixs/bpmn/ui/event/HistoryPropertySection.java
@@ -0,0 +1,62 @@
+package org.imixs.bpmn.ui.event;
+
+import org.eclipse.bpmn2.BaseElement;
+import org.eclipse.bpmn2.IntermediateCatchEvent;
+import org.eclipse.bpmn2.modeler.core.merrimac.clad.AbstractBpmn2PropertySection;
+import org.eclipse.bpmn2.modeler.core.merrimac.clad.AbstractDetailComposite;
+import org.eclipse.bpmn2.modeler.core.merrimac.clad.DefaultPropertySection;
+import org.eclipse.bpmn2.modeler.core.merrimac.dialogs.TextObjectEditor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.imixs.bpmn.model.Property;
+import org.imixs.bpmn.ui.ImixsDetailComposite;
+
+/**
+ * This PorpertySection provides the attributes for History config.
+ * 
+ * @author rsoika
+ *
+ */
+public class HistoryPropertySection extends DefaultPropertySection {
+
+	@Override
+	protected AbstractDetailComposite createSectionRoot() {
+		return new HistoryDetailComposite(this);
+	}
+
+	@Override
+	public AbstractDetailComposite createSectionRoot(Composite parent, int style) {
+		return new HistoryDetailComposite(parent, style);
+	}
+
+	public class HistoryDetailComposite extends ImixsDetailComposite {
+		public HistoryDetailComposite(AbstractBpmn2PropertySection section) {
+			super(section);
+		}
+
+		public HistoryDetailComposite(Composite parent, int style) {
+			super(parent, style);
+		}
+
+		@Override
+		public void createBindings(final EObject be) {
+			if (be == null || !(be instanceof IntermediateCatchEvent)) {
+				return;
+			}
+			setTitle("Process History");
+
+			// Body
+			Property metaData = getPropertyByName((BaseElement) be,
+					"rtfresultlog", "CDATA", "");
+			TextObjectEditor valueEditor = new TextObjectEditor(this, metaData,
+					METADATA_VALUE);
+			valueEditor.setMultiLine(true);
+			valueEditor.setStyle(SWT.MULTI | SWT.V_SCROLL);
+			valueEditor.createControl(this, "History");
+
+		}
+
+	}
+
+}
