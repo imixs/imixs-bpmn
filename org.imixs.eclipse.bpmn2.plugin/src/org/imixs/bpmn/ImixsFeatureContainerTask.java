3096eefcb3bacbd6e9e183315971653714a1d8b7
Ralph Soika <ralph.soika@imixs.com>
2016-06-20 08:44:18 +0200

    issue #53

--- org.imixs.eclipse.bpmn2.plugin/src/org/imixs/bpmn/ImixsFeatureContainerTask.java
+++ org.imixs.eclipse.bpmn2.plugin/src/org/imixs/bpmn/ImixsFeatureContainerTask.java
@@ -1,24 +1,68 @@
 package org.imixs.bpmn;
 
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.bpmn2.BaseElement;
 import org.eclipse.bpmn2.Task;
+import org.eclipse.bpmn2.modeler.core.di.DIUtils;
+import org.eclipse.bpmn2.modeler.core.features.AbstractBpmn2AddFeature;
+import org.eclipse.bpmn2.modeler.core.features.AbstractUpdateBaseElementFeature;
 import org.eclipse.bpmn2.modeler.core.features.CustomShapeFeatureContainer;
+import org.eclipse.bpmn2.modeler.core.features.IFeatureContainer;
+import org.eclipse.bpmn2.modeler.core.features.MultiUpdateFeature;
+import org.eclipse.bpmn2.modeler.core.features.ShowPropertiesFeature;
 import org.eclipse.bpmn2.modeler.core.features.activity.task.AddTaskFeature;
 import org.eclipse.bpmn2.modeler.core.preferences.ShapeStyle;
+import org.eclipse.bpmn2.modeler.core.runtime.CustomTaskImageProvider;
 import org.eclipse.bpmn2.modeler.core.utils.BusinessObjectUtil;
+import org.eclipse.bpmn2.modeler.core.utils.FeatureSupport;
 import org.eclipse.bpmn2.modeler.core.utils.StyleUtil;
 import org.eclipse.bpmn2.modeler.ui.features.activity.task.TaskFeatureContainer;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.graphiti.features.IAddFeature;
 import org.eclipse.graphiti.features.ICreateFeature;
 import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.IReason;
+import org.eclipse.graphiti.features.IUpdateFeature;
 import org.eclipse.graphiti.features.context.IAddContext;
+import org.eclipse.graphiti.features.context.IPictogramElementContext;
+import org.eclipse.graphiti.features.context.IUpdateContext;
+import org.eclipse.graphiti.features.custom.ICustomFeature;
+import org.eclipse.graphiti.features.impl.Reason;
+import org.eclipse.graphiti.mm.GraphicsAlgorithmContainer;
+import org.eclipse.graphiti.mm.PropertyContainer;
+import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
+import org.eclipse.graphiti.mm.algorithms.Image;
+import org.eclipse.graphiti.mm.algorithms.styles.Color;
 import org.eclipse.graphiti.mm.pictograms.ContainerShape;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.mm.pictograms.Shape;
 import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.services.IGaService;
 import org.eclipse.graphiti.services.IPeService;
 import org.eclipse.graphiti.util.ColorConstant;
 import org.eclipse.graphiti.util.IColorConstant;
+import org.eclipse.jface.resource.ImageDescriptor;
+
+import org.imixs.bpmn.model.Value;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * Imixs ProcessEntity task container
@@ -27,12 +71,15 @@ import org.eclipse.graphiti.util.IColorConstant;
  *
  */
 public class ImixsFeatureContainerTask extends CustomShapeFeatureContainer {
- 
+
 	// these values must match what's in the plugin.xml
 	public final static String PROCESSENTITY_TASK_ID = "org.imixs.workflow.bpmn.ProcessEntityTask";
-	private static final IColorConstant PROCESSENTITY_BACKGROUND = new ColorConstant(
-			144, 176, 224);
-  
+	private static final IColorConstant PROCESSENTITY_BACKGROUND = new ColorConstant(144, 176, 224);
+
+	private static final IColorConstant PROCESSENTITY_BACKGROUND_ACL = new ColorConstant(144, 106, 224);
+
+	private List<String> registeredTaskIcons;
+
 	/**
 	 * 
 	 * This method inspects the object to determine what its custom task ID
@@ -50,24 +97,9 @@ public class ImixsFeatureContainerTask extends CustomShapeFeatureContainer {
 		return null;
 	}
 
-	/**
-	 * overwrite task features displayed during mouse over
-	 * 
-	 * Breaks plugin !!!
-	 * 
-	 * @see https://bugs.eclipse.org/bugs/show_bug.cgi?id=471219
-	 */
-//	@Override
-//	public ICustomFeature[] getCustomFeatures(IFeatureProvider fp) {
-//		// test if it now works...
-//				 return new ICustomFeature[] { new ShowPropertiesFeature(fp) };
-//				//return super.getCustomFeatures(fp);
-//				 
-//		//return getFeatureContainer(fp).getCustomFeatures(fp);
-//	}
- 
 	@Override
 	protected TaskFeatureContainer createFeatureContainer(IFeatureProvider fp) {
+
 		return new TaskFeatureContainer() {
 
 			/**
@@ -75,34 +107,30 @@ public class ImixsFeatureContainerTask extends CustomShapeFeatureContainer {
 			 * class . Typically you will want to override the decorateShape()
 			 * method which allows you to customize the graphical representation
 			 * of this Custom Task figure.
-			 */ 
+			 */
 			@Override
 			public IAddFeature getAddFeature(IFeatureProvider fp) {
 				return new AddTaskFeature(fp) {
 
-					@SuppressWarnings("unused")
 					@Override
-					protected void decorateShape(IAddContext context,
-							ContainerShape containerShape, Task businessObject) {
-						super.decorateShape(context, containerShape,
-								businessObject);
+					protected void decorateShape(IAddContext context, ContainerShape containerShape,
+							Task businessObject) {
+						super.decorateShape(context, containerShape, businessObject);
 
-						IGaService gaService = Graphiti.getGaService();
-						IPeService peService = Graphiti.getPeService();
-						// Change the size of the default TextAnnotation
-						// selection rectangle
-						// Rectangle selectionRect =
-						// (Rectangle)containerShape.getGraphicsAlgorithm();
-						// int width = 140;
-						// int height = 60;
-						// selectionRect.setWidth(width);
-						// selectionRect.setHeight(height);
+						// test current ACL setting...
+						Value valueUpdateACL = ImixsBPMNPlugin.getItemValueByName(businessObject, "keyUpdateACL",
+								"xs:boolean", "false");
+						System.out.println("Task - ACL=" + valueUpdateACL);
 
-						setFillColor(containerShape);
+						// ContainerShape komischesDing = (ContainerShape)
+						// ((IPictogramElementContext)
+						// context).getPictogramElement();
+
+						updateShapeStyle(containerShape);
 
 						// suggest next free processID
 						ImixsBPMNPlugin.suggestNextProcessId(businessObject);
-						
+
 						// add a notifyChangeAdapter to validate the ActiviytID
 						businessObject.eAdapters().add(new ImixsTaskAdapter());
 
@@ -110,8 +138,6 @@ public class ImixsFeatureContainerTask extends CustomShapeFeatureContainer {
 				};
 			}
 
-			
-		
 			/**
 			 * this MUST be overridden if you intend to add extension attributes
 			 * to your business object (bpmn2 element) - see the code example
@@ -119,39 +145,196 @@ public class ImixsFeatureContainerTask extends CustomShapeFeatureContainer {
 			 * palette by overriding getCreateImageId() and
 			 * getCreateLargeImageId() in your Create Feature.
 			 */
-			 @Override
-			 public ICreateFeature getCreateFeature(IFeatureProvider fp) {
-				 return new CreateTaskFeature(fp) {
-				 };
-			 }
+			@Override
+			public ICreateFeature getCreateFeature(IFeatureProvider fp) {
+				return new CreateTaskFeature(fp) {
+				};
+			}
+
+			/**
+			 * This method updates the layout in case the ACL property of an
+			 * Imixs Task Element changed
+			 */
+			@Override
+			public IUpdateFeature getUpdateFeature(IFeatureProvider fp) {
+				MultiUpdateFeature multiUpdate = (MultiUpdateFeature) super.getUpdateFeature(fp);
+				multiUpdate.addFeature(new AbstractUpdateBaseElementFeature<Task>(fp) {
+
+					@Override
+					public boolean update(IUpdateContext context) {
+						DIUtils.updateDIShape(context.getPictogramElement());
+
+						System.out.println("update....");
+
+						updateShapeStyle((ContainerShape) context.getPictogramElement());
+						return true;
+					}
+
+					@Override
+					public IReason updateNeeded(IUpdateContext context) {
+
+						IReason reason = super.updateNeeded(context);
+						if (reason.toBoolean())
+							return reason;
+
+						PictogramElement pe = context.getPictogramElement();
+						BaseElement ta = (BaseElement) getBusinessObjectForPictogramElement(pe);
+
+						String oldLayoutStatus = getLastLayoutStatus(context);
+						String newLayoutStatus = getNewLayoutStatus(pe, ta);
+
+						// update is needed if the property has changed....
+						if (!oldLayoutStatus.equals(newLayoutStatus)) {
+
+							System.out.println("newLayoutStatus=" + newLayoutStatus + " -> update needed!!");
+							// getDiagramBehavior().refreshContent();
+							return Reason.createTrueReason("layoutstatus.property changed");
+
+						}
+						return Reason.createFalseReason("");
+					}
+
+				});
+
+				return multiUpdate;
+			}
 
 			/**
 			 * Common method used to set the fill color for Imixs CustomTask
 			 * figure. This method is called by both the CreateFeature and the
 			 * UpdateFeature.
 			 * 
+			 * The method evaluates the property of the business object to
+			 * compute the layout
+			 * 
 			 * @param containerShape
 			 *            - the ContainerShape that corresponds to the Task.
 			 */
-			private void setFillColor(ContainerShape containerShape) {
-				Task ta = BusinessObjectUtil.getFirstElementOfType(
-						containerShape, Task.class);
+			private void updateShapeStyle(ContainerShape containerShape) {
+				Task ta = BusinessObjectUtil.getFirstElementOfType(containerShape, Task.class);
 				if (ta != null) {
-					// ExtendedPropertiesAdapter adapter =
-					// ExtendedPropertiesAdapter.adapt(ta);
-					// Boolean attributeValue =
-					// (Boolean)adapter.getFeatureDescriptor("evaluate").getValue();
-					Shape shape = containerShape.getChildren().get(0);
-					ShapeStyle ss = new ShapeStyle();
+					
+					// compute current width to place icons into the upper right corner..
+					int width = containerShape.getGraphicsAlgorithm().getWidth();
+					int xPos=width-20;
 
-					ss.setDefaultColors(PROCESSENTITY_BACKGROUND);
-					StyleUtil.applyStyle(shape.getGraphicsAlgorithm(), ta, ss);
-					// Graphiti.getPeService().setPropertyValue(containerShape,
-					// "evaluate.property", propertyValue);
+					PictogramElement pe = containerShape.getGraphicsAlgorithm().getPictogramElement();
+					String newLayoutStatus = getNewLayoutStatus(pe, ta);
+
+					Shape shape = containerShape.getChildren().get(0);
+					
+					// now we need to clear all existing children of that shape...
+					shape.getGraphicsAlgorithm().getGraphicsAlgorithmChildren().clear();
+					
+					
+					// set background color
+					ShapeStyle shapeStyle = new ShapeStyle();
+					shapeStyle.setDefaultColors(PROCESSENTITY_BACKGROUND);
+
+					
+					// add the default image into the upper left corner....
+					Image imga = loadCustomTaskIcon("process-bubble.png", shape.getGraphicsAlgorithm());
+					Graphiti.getGaService().setLocationAndSize(imga, 2, 2, 24, 24);
+
+					// Add 1. custom Image
+					if (newLayoutStatus.contains("keyupdateacl=true")) {
+						Image img = loadCustomTaskIcon("user_group.gif", shape.getGraphicsAlgorithm());
+						Graphiti.getGaService().setLocation(img,xPos, 2);
+						xPos=xPos-20;
+					}
+
+					// Add 2. custom Image
+					if (newLayoutStatus.contains("txteditorid=")) {
+						Image img = loadCustomTaskIcon("form.gif", shape.getGraphicsAlgorithm());
+						Graphiti.getGaService().setLocation(img,xPos, 2);
+					}
+
+					StyleUtil.applyStyle(shape.getGraphicsAlgorithm(), ta, shapeStyle);
+
+					// finally we update the new property value stored in the
+					System.out.println("set new layout status: ");
+					FeatureSupport.setPropertyValue(containerShape, "layoutstatus.property", newLayoutStatus);
 				}
+
 			}
 
+			/**
+			 * This method returns the current status of the layout for the
+			 * custom shape. This layout status is used to verify if an updates
+			 * is needed. The layout status is saved in the property
+			 * 'lyoutstatus.property' of the feature context.
+			 * 
+			 * @see updateNeeded()
+			 * @return
+			 */
+			private String getLastLayoutStatus(IUpdateContext context) {
+				PictogramElement pe = context.getPictogramElement();
+				String layoutStatus = FeatureSupport.getPropertyValue(pe, "layoutstatus.property");
+				if (layoutStatus == null || layoutStatus.isEmpty()) {
+					System.out.println("layoutstatus.property not yet created...");
+					layoutStatus = "";
+				}
+
+				return layoutStatus;
+			}
+
+			/**
+			 * This method computes the new layout status of the current
+			 * business object and the size of the custom shape element.
+			 * 
+			 * @return
+			 */
+			private String getNewLayoutStatus(PictogramElement pe, BaseElement task) {
+				String result = "";
+
+				// Now evaluate the property 'keyUpdateACL' of the task
+				// object
+				Value valueUpdateACL = ImixsBPMNPlugin.getItemValueByName(task, "keyUpdateACL", "xs:boolean", "false");
+				result += "keyupdateacl=" + valueUpdateACL.getValue() + ";";
+
+				// evaluate the property 'txteditorid' of the task object
+				Value valueEditorId = ImixsBPMNPlugin.getItemValueByName(task, "txteditorid", "xs:string", "");
+				if (!valueEditorId.getValue().isEmpty()) {
+					result += "txteditorid=" + valueEditorId.getValue() + ";";
+				}
+
+				// evaluate element width
+				int width = pe.getGraphicsAlgorithm().getWidth();
+				result += "width=" + width + ";";
+
+				return result;
+			}
 		};
 	}
 
+	/**
+	 * This method loads a custom image and register it automatically on first
+	 * load.
+	 * 
+	 * @param fileName
+	 * @param ga
+	 * @return
+	 */
+	private Image loadCustomTaskIcon(String fileName, GraphicsAlgorithm ga) {
+		if (registeredTaskIcons == null) {
+			registeredTaskIcons = new ArrayList<String>();
+		}
+
+		String imageId = customTaskDescriptor.getImageId(fileName, CustomTaskImageProvider.IconSize.LARGE);
+		if (!registeredTaskIcons.contains(imageId)) {
+			String filename = "/icons/large/" + fileName;
+			URL url = getClass().getClassLoader().getResource(filename);
+			ImageDescriptor descriptor = ImageDescriptor.createFromURL(url);
+			CustomTaskImageProvider.registerImage(imageId, descriptor);
+			registeredTaskIcons.add(imageId);
+		}
+		Image img = Graphiti.getGaService().createImage(ga, imageId);
+		img.setProportional(false);
+		img.setWidth(16);
+		img.setHeight(16);
+		img.setStretchH(true);
+		img.setStretchV(true);
+
+		return img;
+	}
 }
--- org.imixs.eclipse.bpmn2.plugin/src/org/imixs/bpmn/ui/task/ACLPropertySection.java
+++ org.imixs.eclipse.bpmn2.plugin/src/org/imixs/bpmn/ui/task/ACLPropertySection.java
@@ -3,9 +3,20 @@ package org.imixs.bpmn.ui.task;
 import java.util.Map;
 
 import org.eclipse.bpmn2.BaseElement;
+import org.eclipse.bpmn2.modeler.core.adapters.AbstractAdapter;
+import org.eclipse.bpmn2.modeler.core.di.DIUtils;
 import org.eclipse.bpmn2.modeler.core.merrimac.clad.AbstractBpmn2PropertySection;
 import org.eclipse.bpmn2.modeler.core.merrimac.clad.AbstractDetailComposite;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.emf.common.notify.impl.NotificationImpl;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.transaction.ResourceSetChangeEvent;
+import org.eclipse.graphiti.features.IUpdateFeature;
+import org.eclipse.graphiti.features.context.impl.UpdateContext;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.eclipse.graphiti.ui.editor.DiagramEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
@@ -15,6 +26,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.imixs.bpmn.AbstractImixsAdapter;
 import org.imixs.bpmn.ImixsBPMNPlugin;
 import org.imixs.bpmn.model.Item;
 import org.imixs.bpmn.model.Value;
@@ -54,23 +66,52 @@ public class ACLPropertySection extends AbstractProcessPropertySection {
 		}
 
 		@Override
-		public void createBindings(final EObject be) {
-			if (!ImixsBPMNPlugin.isImixsTask(businessObject)){
-				return ;
-			}
-		
-			
-			setTitle("Access Control List (Task)");
-			
-			
-			// Create checkbox to hide acl elements...
-			Value valueUpdateACL = ImixsBPMNPlugin.getItemValueByName((BaseElement) be,
-					"keyUpdateACL", "xs:boolean", "false");
-			BooleanEditor bEditor = new BooleanEditor(this, valueUpdateACL);
+		public void resourceSetChanged(ResourceSetChangeEvent event) {
+			super.resourceSetChanged(event);
+		}
 
+		@Override
+		public void createBindings(final EObject be) {
+			if (!ImixsBPMNPlugin.isImixsTask(businessObject)) {
+				return;
+			}
+
+			setTitle("Access Control List (Task)");
+
+			// Create checkbox to hide acl elements...
+			Value valueUpdateACL = ImixsBPMNPlugin.getItemValueByName((BaseElement) be, "keyUpdateACL", "xs:boolean",
+					"false");
+
+		/*	*/
+			valueUpdateACL.eAdapters().add(new AdapterImpl() {
+				public void notifyChanged(Notification notification) {
+					int type = notification.getEventType();
+
+					if (type == Notification.SET) {
+						Object newValue = notification.getNewValue();
+						if (newValue != null) {
+							System.out.println("notify type=SET   value=" + newValue);
+
+							PictogramElement pe = null;
+							PictogramElement[] pictogramElementList = getDiagramEditor().getSelectedPictogramElements();
+							if (pictogramElementList != null && pictogramElementList.length > 0) {
+								pe = pictogramElementList[0];
+								UpdateContext updateContext = new UpdateContext(pe);
+								IUpdateFeature iUpdateFeature = getDiagramEditor().getDiagramTypeProvider()
+										.getFeatureProvider().getUpdateFeature(updateContext);
+								if (iUpdateFeature.canUpdate(updateContext))
+									iUpdateFeature.update(updateContext);
+							}
+
+						}
+					}
+				}
+			});
+			
+			
+			BooleanEditor bEditor = new BooleanEditor(this, valueUpdateACL);
 			bEditor.createControl(attributesComposite, "Update ACL");
 			bEditor.addSelectionListener(new SelectionListener() {
-
 				@Override
 				public void widgetSelected(SelectionEvent e) {
 					boolean checked = ((Button) e.getSource()).getSelection();
@@ -81,79 +122,58 @@ public class ACLPropertySection extends AbstractProcessPropertySection {
 				public void widgetDefaultSelected(SelectionEvent arg0) {
 					// no op
 				}
-
 			});
 
-			
-			
-
-		
 			// get Name Fields...
-			Map<String, String> optionList = ImixsBPMNPlugin
-					.getOptionListFromDefinition((BaseElement) be,
-							"txtFieldMapping");
+			Map<String, String> optionList = ImixsBPMNPlugin.getOptionListFromDefinition((BaseElement) be,
+					"txtFieldMapping");
 
 			aclComposite = toolkit.createComposite(attributesComposite);
 
-			aclComposite.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true,
-					true, 3, 1));
+			aclComposite.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, true, 3, 1));
 
 			aclComposite.setLayout(new GridLayout(6, true));
 
 			// create a separate labels
-			Label labelWidget = getToolkit()
-					.createLabel(aclComposite, "Owner:");
+			Label labelWidget = getToolkit().createLabel(aclComposite, "Owner:");
 			labelWidget.setFont(new Font(getDisplay(), "Arial", 11, SWT.BOLD));
 
-			labelWidget.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false,
-					false, 2, 1));
+			labelWidget.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false, 2, 1));
 
-			labelWidget = getToolkit()
-					.createLabel(aclComposite, "Read Access:");
+			labelWidget = getToolkit().createLabel(aclComposite, "Read Access:");
 			labelWidget.setFont(new Font(getDisplay(), "Arial", 11, SWT.BOLD));
-			labelWidget.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false,
-					false, 2, 1));
-			labelWidget = getToolkit().createLabel(aclComposite,
-					"Write Access:");
+			labelWidget.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false, 2, 1));
+			labelWidget = getToolkit().createLabel(aclComposite, "Write Access:");
 			labelWidget.setFont(new Font(getDisplay(), "Arial", 11, SWT.BOLD));
-			labelWidget.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false,
-					false, 2, 1));
+			labelWidget.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false, 2, 1));
 
-			Item item = ImixsBPMNPlugin.getItemByName((BaseElement) be,
-					"keyOwnershipFields", null);
+			Item item = ImixsBPMNPlugin.getItemByName((BaseElement) be, "keyOwnershipFields", null);
 			CheckBoxEditor aEditor = new CheckBoxEditor(this, item, optionList);
 			aEditor.createControl(aclComposite, null);
 
-			item = ImixsBPMNPlugin.getItemByName((BaseElement) be,
-					"keyAddReadFields", null);
+			item = ImixsBPMNPlugin.getItemByName((BaseElement) be, "keyAddReadFields", null);
 			aEditor = new CheckBoxEditor(this, item, optionList);
 			aEditor.createControl(aclComposite, null);
 
-			item = ImixsBPMNPlugin.getItemByName((BaseElement) be,
-					"keyAddWriteFields", null);
+			item = ImixsBPMNPlugin.getItemByName((BaseElement) be, "keyAddWriteFields", null);
 			aEditor = new CheckBoxEditor(this, item, optionList);
 			aEditor.createControl(aclComposite, null);
 
-			item = ImixsBPMNPlugin.getItemByName((BaseElement) be,
-					"namOwnershipNames", null);
+			item = ImixsBPMNPlugin.getItemByName((BaseElement) be, "namOwnershipNames", null);
 			ListEditor aListEditor = new ListEditor(this, item);
 			aListEditor.createControl(aclComposite, null);
 
-			item = ImixsBPMNPlugin.getItemByName((BaseElement) be,
-					"namAddReadAccess", null);
+			item = ImixsBPMNPlugin.getItemByName((BaseElement) be, "namAddReadAccess", null);
 			aListEditor = new ListEditor(this, item);
 			aListEditor.createControl(aclComposite, null);
 
-			item = ImixsBPMNPlugin.getItemByName((BaseElement) be,
-					"namAddWriteAccess", null);
+			item = ImixsBPMNPlugin.getItemByName((BaseElement) be, "namAddWriteAccess", null);
 			aListEditor = new ListEditor(this, item);
 			aListEditor.createControl(aclComposite, null);
 
-		
-			
 			// hide section if no update...
 			aclComposite.setVisible("true".equals(valueUpdateACL.getValue()));
-			
+
 		}
 
 	}
