3f7ba41403d6896cb2bf14cb55e61482c36268b9
Ralph Soika <ralph.soika@imixs.com>
2016-06-20 11:31:30 +0200

    issue #53

--- org.imixs.eclipse.bpmn2.plugin/src/org/imixs/bpmn/ImixsFeatureContainerTask.java
+++ org.imixs.eclipse.bpmn2.plugin/src/org/imixs/bpmn/ImixsFeatureContainerTask.java
@@ -1,68 +1,45 @@
 package org.imixs.bpmn;
 
-import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.bpmn2.BaseElement;
 import org.eclipse.bpmn2.Task;
 import org.eclipse.bpmn2.modeler.core.di.DIUtils;
-import org.eclipse.bpmn2.modeler.core.features.AbstractBpmn2AddFeature;
 import org.eclipse.bpmn2.modeler.core.features.AbstractUpdateBaseElementFeature;
 import org.eclipse.bpmn2.modeler.core.features.CustomShapeFeatureContainer;
-import org.eclipse.bpmn2.modeler.core.features.IFeatureContainer;
 import org.eclipse.bpmn2.modeler.core.features.MultiUpdateFeature;
-import org.eclipse.bpmn2.modeler.core.features.ShowPropertiesFeature;
 import org.eclipse.bpmn2.modeler.core.features.activity.task.AddTaskFeature;
+import org.eclipse.bpmn2.modeler.core.model.ModelDecorator;
 import org.eclipse.bpmn2.modeler.core.preferences.ShapeStyle;
 import org.eclipse.bpmn2.modeler.core.runtime.CustomTaskImageProvider;
 import org.eclipse.bpmn2.modeler.core.utils.BusinessObjectUtil;
 import org.eclipse.bpmn2.modeler.core.utils.FeatureSupport;
 import org.eclipse.bpmn2.modeler.core.utils.StyleUtil;
 import org.eclipse.bpmn2.modeler.ui.features.activity.task.TaskFeatureContainer;
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.emf.common.notify.Adapter;
-import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.TreeIterator;
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EOperation;
-import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.graphiti.features.IAddFeature;
 import org.eclipse.graphiti.features.ICreateFeature;
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.IReason;
 import org.eclipse.graphiti.features.IUpdateFeature;
 import org.eclipse.graphiti.features.context.IAddContext;
-import org.eclipse.graphiti.features.context.IPictogramElementContext;
 import org.eclipse.graphiti.features.context.IUpdateContext;
-import org.eclipse.graphiti.features.custom.ICustomFeature;
 import org.eclipse.graphiti.features.impl.Reason;
-import org.eclipse.graphiti.mm.GraphicsAlgorithmContainer;
-import org.eclipse.graphiti.mm.PropertyContainer;
 import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
 import org.eclipse.graphiti.mm.algorithms.Image;
-import org.eclipse.graphiti.mm.algorithms.styles.Color;
+import org.eclipse.graphiti.mm.algorithms.Text;
+import org.eclipse.graphiti.mm.algorithms.styles.Orientation;
 import org.eclipse.graphiti.mm.pictograms.ContainerShape;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.mm.pictograms.Shape;
 import org.eclipse.graphiti.services.Graphiti;
-import org.eclipse.graphiti.services.IGaService;
-import org.eclipse.graphiti.services.IPeService;
 import org.eclipse.graphiti.util.ColorConstant;
 import org.eclipse.graphiti.util.IColorConstant;
 import org.eclipse.jface.resource.ImageDescriptor;
-
 import org.imixs.bpmn.model.Value;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.FrameworkUtil;
 
 /**
  * Imixs ProcessEntity task container
@@ -75,9 +52,6 @@ public class ImixsFeatureContainerTask extends CustomShapeFeatureContainer {
 	// these values must match what's in the plugin.xml
 	public final static String PROCESSENTITY_TASK_ID = "org.imixs.workflow.bpmn.ProcessEntityTask";
 	private static final IColorConstant PROCESSENTITY_BACKGROUND = new ColorConstant(144, 176, 224);
-
-	private static final IColorConstant PROCESSENTITY_BACKGROUND_ACL = new ColorConstant(144, 106, 224);
-
 	private List<String> registeredTaskIcons;
 
 	/**
@@ -117,15 +91,6 @@ public class ImixsFeatureContainerTask extends CustomShapeFeatureContainer {
 							Task businessObject) {
 						super.decorateShape(context, containerShape, businessObject);
 
-						// test current ACL setting...
-						Value valueUpdateACL = ImixsBPMNPlugin.getItemValueByName(businessObject, "keyUpdateACL",
-								"xs:boolean", "false");
-						System.out.println("Task - ACL=" + valueUpdateACL);
-
-						// ContainerShape komischesDing = (ContainerShape)
-						// ((IPictogramElementContext)
-						// context).getPictogramElement();
-
 						updateShapeStyle(containerShape);
 
 						// suggest next free processID
@@ -164,8 +129,7 @@ public class ImixsFeatureContainerTask extends CustomShapeFeatureContainer {
 					public boolean update(IUpdateContext context) {
 						DIUtils.updateDIShape(context.getPictogramElement());
 
-						System.out.println("update....");
-
+						//System.out.println("update....");
 						updateShapeStyle((ContainerShape) context.getPictogramElement());
 						return true;
 					}
@@ -186,8 +150,7 @@ public class ImixsFeatureContainerTask extends CustomShapeFeatureContainer {
 						// update is needed if the property has changed....
 						if (!oldLayoutStatus.equals(newLayoutStatus)) {
 
-							System.out.println("newLayoutStatus=" + newLayoutStatus + " -> update needed!!");
-							// getDiagramBehavior().refreshContent();
+							//System.out.println("newLayoutStatus=" + newLayoutStatus);
 							return Reason.createTrueReason("layoutstatus.property changed");
 
 						}
@@ -213,25 +176,25 @@ public class ImixsFeatureContainerTask extends CustomShapeFeatureContainer {
 			private void updateShapeStyle(ContainerShape containerShape) {
 				Task ta = BusinessObjectUtil.getFirstElementOfType(containerShape, Task.class);
 				if (ta != null) {
-					
-					// compute current width to place icons into the upper right corner..
+
+					// compute current width to place icons into the upper right
+					// corner..
 					int width = containerShape.getGraphicsAlgorithm().getWidth();
-					int xPos=width-20;
+					int height = containerShape.getGraphicsAlgorithm().getHeight();
+					int xPos = width - 20;
 
 					PictogramElement pe = containerShape.getGraphicsAlgorithm().getPictogramElement();
 					String newLayoutStatus = getNewLayoutStatus(pe, ta);
 
 					Shape shape = containerShape.getChildren().get(0);
-					
-					// now we need to clear all existing children of that shape...
+					// now we need to clear all existing children of that
+					// shape...
 					shape.getGraphicsAlgorithm().getGraphicsAlgorithmChildren().clear();
-					
-					
+
 					// set background color
 					ShapeStyle shapeStyle = new ShapeStyle();
 					shapeStyle.setDefaultColors(PROCESSENTITY_BACKGROUND);
 
-					
 					// add the default image into the upper left corner....
 					Image imga = loadCustomTaskIcon("process-bubble.png", shape.getGraphicsAlgorithm());
 					Graphiti.getGaService().setLocationAndSize(imga, 2, 2, 24, 24);
@@ -239,20 +202,34 @@ public class ImixsFeatureContainerTask extends CustomShapeFeatureContainer {
 					// Add 1. custom Image
 					if (newLayoutStatus.contains("keyupdateacl=true")) {
 						Image img = loadCustomTaskIcon("user_group.gif", shape.getGraphicsAlgorithm());
-						Graphiti.getGaService().setLocation(img,xPos, 2);
-						xPos=xPos-20;
+						Graphiti.getGaService().setLocation(img, xPos, 2);
+						xPos = xPos - 20;
 					}
 
 					// Add 2. custom Image
 					if (newLayoutStatus.contains("txteditorid=")) {
 						Image img = loadCustomTaskIcon("form.gif", shape.getGraphicsAlgorithm());
-						Graphiti.getGaService().setLocation(img,xPos, 2);
+						Graphiti.getGaService().setLocation(img, xPos, 2);
+					}
+
+					// we take the second shape to add the text
+					if (newLayoutStatus.contains("processid=")) {
+						// compute id
+						String sID=newLayoutStatus.substring( newLayoutStatus.indexOf("processid=")+10);
+						sID=sID.substring(0, sID.indexOf(";")-0);
+						Shape textShape = containerShape.getChildren().get(1);
+						Text text = Graphiti.getGaService().createText(textShape, "ID: " + sID);
+						text.setHorizontalAlignment(Orientation.ALIGNMENT_RIGHT);
+						text.setVerticalAlignment(Orientation.ALIGNMENT_BOTTOM);
+						text.setFont(Graphiti.getGaService().manageDefaultFont(DIUtils.getDiagram(ta), false, false));
+						Graphiti.getGaService().setLocationAndSize(text, width - 105, height - 20, 100, 20);
+						// TODO we are not sure if it is correct to change the
+						// active mode of an existing shape!
+						textShape.setActive(false);
 					}
 
 					StyleUtil.applyStyle(shape.getGraphicsAlgorithm(), ta, shapeStyle);
-
 					// finally we update the new property value stored in the
-					System.out.println("set new layout status: ");
 					FeatureSupport.setPropertyValue(containerShape, "layoutstatus.property", newLayoutStatus);
 				}
 
@@ -286,6 +263,10 @@ public class ImixsFeatureContainerTask extends CustomShapeFeatureContainer {
 			 */
 			private String getNewLayoutStatus(PictogramElement pe, BaseElement task) {
 				String result = "";
+
+				EStructuralFeature feature = ModelDecorator.getAnyAttribute(task, "processid");
+				Integer currentProcessID = (Integer) task.eGet(feature);
+				result += "processid=" + currentProcessID + ";";
 
 				// Now evaluate the property 'keyUpdateACL' of the task
 				// object
